e406bf10d9479eb73ca75f5b67be9ce176102b3d
Matt <matthew.t.kissinger@gmail.com>
2023-08-07 22:20:16 -0700

    push new message to messages array from /new form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando", 
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
+/* New Message page */
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message Page' });
+})
+
+router.post('/new', (req, res, next) => {
+  console.log(req.body.messageUser);
+  console.log(req.body.messageText);
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date()
+  })
+  res.redirect('/');
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
